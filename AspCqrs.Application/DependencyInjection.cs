df85f1d2241b1623f61981844233b74d30a6bfbe
Farah <farahexa2020@gmail.com>
2021-07-19 13:24:32 +0300

    Add validation behaviour

--- AspCqrs.Application/DependencyInjection.cs
+++ AspCqrs.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using AspCqrs.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspCqrs.Application
@@ -8,6 +11,10 @@ namespace AspCqrs.Application
         public static void AddApplication(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         }
     }
 }
---
+++ AspCqrs.Application/TodoItems/Commands/CreateTodoItemCommandValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+
+namespace AspCqrs.Application.TodoItems.Commands
+{
+    public class CreateTodoItemCommandValidator : AbstractValidator<CreateTodoItemCommand>
+    {
+        public CreateTodoItemCommandValidator()
+        {
+            RuleFor(v => v.Title)
+                .NotEmpty()
+                .MaximumLength(64)
+                .MinimumLength(8);
+
+            RuleFor(v => v.UserId)
+                .NotEmpty();
+        }
+    }
+}
