420ceb56a523e4c4af9e4c9977610b004dc069b8
Farah <farahexa2020@gmail.com>
2021-07-23 22:36:22 +0300

    Add update command id validation

--- AspCqrs.Api/Controllers/CrudControllerBase.cs
+++ AspCqrs.Api/Controllers/CrudControllerBase.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AspCqrs.Application.Common.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspCqrs.Api.Controllers
 {
-    public abstract class CrudControllerBase<TGetAllQuery, TGetByIdQuery, TCreateCommand, TUpdateCommand, TDeleteCommand> : ApiControllerBase
+    public abstract class CrudControllerBase<TGetAllQuery, TGetByIdQuery, TCreateCommand, TUpdateCommand,
+        TDeleteCommand> : ApiControllerBase
     {
         [HttpGet]
         public async Task<IActionResult> Get([FromQuery] TGetAllQuery getAllQuery)
@@ -13,16 +16,16 @@ namespace AspCqrs.Api.Controllers
 
             return Ok(result);
         }
-        
+
         [HttpGet("{id}")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
             var query = Activator.CreateInstance(typeof(TGetByIdQuery), id);
-            
+
             if (query == null) throw new ArgumentException("Null query");
-            
+
             var result = await Mediator.Send(query);
-            
+
             return Ok(result);
         }
 
@@ -37,8 +40,18 @@ namespace AspCqrs.Api.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] TUpdateCommand updateCommand)
         {
-            typeof(TUpdateCommand).GetProperty("Id")?.SetValue(updateCommand, id, null);
+            var result = int.TryParse(typeof(TUpdateCommand).GetProperty("Id")?.GetValue(updateCommand)?.ToString(),
+                out var bodyId);
 
+            if (!result || bodyId != id)
+                throw new FailedRequestException(new Dictionary<string, string[]>
+                {
+                    {
+                        "IdMisMatch",
+                        new[] {$"Route ID value ({id}) does not match body ID Property value ({bodyId})."}
+                    }
+                });
+            
             await Mediator.Send(updateCommand);
 
             return NoContent();
@@ -48,9 +61,9 @@ namespace AspCqrs.Api.Controllers
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
             var command = Activator.CreateInstance(typeof(TDeleteCommand), id);
-            
+
             if (command == null) throw new ArgumentException("Null command");
-            
+
             await Mediator.Send(command);
 
             return NoContent();
