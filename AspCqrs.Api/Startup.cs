29a34c9c8b98aebd964f0b9faa68c6b9d86010cb
Farah <farahexa2020@gmail.com>
2021-08-04 08:09:14 +0300

    Add CORS policy

--- AspCqrs.Api/Startup.cs
+++ AspCqrs.Api/Startup.cs
@@ -21,9 +21,12 @@ namespace AspCqrs.Api
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            CorsPolicy = configuration.GetSection("Cors").GetValue<string>("Policy");
         }
 
         public IConfiguration Configuration { get; }
+
+        public string CorsPolicy { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,44 +34,56 @@ namespace AspCqrs.Api
             services.AddControllers();
 
             services.AddHttpContextAccessor();
-            
+
             services.AddSwaggerGen(c =>
             {
                 c.SchemaFilter<SwaggerKeyPropertySchemaFilter>();
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspCqrs.Api", Version = "v1" });
-                
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()  
-                {  
-                    Name = "Authorization",  
-                    Type = SecuritySchemeType.ApiKey,  
-                    Scheme = "Bearer",  
-                    BearerFormat = "JWT",  
-                    In = ParameterLocation.Header,  
-                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",  
-                });  
-                
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement  
-                {  
-                    {  
-                        new OpenApiSecurityScheme  
-                        {  
-                            Reference = new OpenApiReference  
-                            {  
-                                Type = ReferenceType.SecurityScheme,  
-                                Id = "Bearer"  
-                            }  
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "AspCqrs.Api", Version = "v1"});
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
                         },
                         Array.Empty<string>()
-                    }  
+                    }
+                });
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsPolicy, builder =>
+                {
+                    builder.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .SetIsOriginAllowed(host => true)
+                        .AllowCredentials();
                 });
             });
 
             services.AddSignalR();
 
             services.AddInfrastructure(Configuration);
-            
+
             services.AddApplication();
-            
+
             services.AddScoped<ICurrentUserService, CurrentUserService>();
 
             services.AddScoped<IStreamService<TodoItemDto>, TodoItemStreamService>();
@@ -83,12 +98,14 @@ namespace AspCqrs.Api
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspCqrs.Api v1"));
             }
-            
+
             Seeder.Seed(app.ApplicationServices.CreateScope().ServiceProvider);
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(CorsPolicy);
 
             app.UseAuthentication();
             app.UseAuthorization();
@@ -99,7 +116,7 @@ namespace AspCqrs.Api
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
 
-                endpoints.MapHub<TodoItemHub>("hubs/TodoItems");
+                endpoints.MapHub<TodoItemHub>("Hubs/TodoItems");
             });
         }
     }
